2680a3ff0ab2858076a62b5bbe7446222247b186
cartercroft <carterncroft@gmail.com>
2021-05-19 19:53:42 -0500

    Tweaked solution to include index value for each iteration.

--- TenDigitFibonacci/TenDigitFibonacci/Program.cs
+++ TenDigitFibonacci/TenDigitFibonacci/Program.cs
@@ -10,19 +10,22 @@ namespace TenDigitFibonacci
     {
         static void Main(string[] args)
         {
-            ulong prevTerm = 1;
-            ulong currentTerm = 1;
-            int index = 2;
+            ulong prevTerm = 1;//F(1)
+            int index = 1;//Starting with terms F(1) = 1, F(2) = 1.
+            Console.WriteLine(1 + "\tDigits: " + 1 + "\tIndex: " + index);
+
+            ulong currentTerm = 1;//F(2)
+            index = 2;
             int digits = currentTerm.ToString().Count();
+
             while (digits < 10)
             {
-                index++; //increment index
-
-                Console.WriteLine(currentTerm + " Digits: " + digits);
+                Console.WriteLine(currentTerm + "\tDigits: " + digits + "\tIndex: " + index);
                 ulong temp = currentTerm; //Holds value for currentTerm before it is updated
                 currentTerm = prevTerm + currentTerm;
                 prevTerm = temp; //Receives value held by currentTerm when entering loop
                 digits = currentTerm.ToString().Count();
+                index++; //increment index
             }
             Console.WriteLine("Number: " + currentTerm + "\tDigits: " + digits + "\tIndex: " + index);
         }
