30edebcfc17b5194ff8ebe2440c65a64977dc986
cartercroft <carterncroft@gmail.com>
2021-05-19 19:46:02 -0500

    Initial Commit - Added and completed TenDigitFibonacci

---
+++ TenDigitFibonacci/TenDigitFibonacci/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TenDigitFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong prevTerm = 1;
+            ulong currentTerm = 1;
+            int index = 2;
+            int digits = currentTerm.ToString().Count();
+            while (digits < 10)
+            {
+                index++; //increment index
+
+                Console.WriteLine(currentTerm + " Digits: " + digits);
+                ulong temp = currentTerm; //Holds value for currentTerm before it is updated
+                currentTerm = prevTerm + currentTerm;
+                prevTerm = temp; //Receives value held by currentTerm when entering loop
+                digits = currentTerm.ToString().Count();
+            }
+            Console.WriteLine("Number: " + currentTerm + "\tDigits: " + digits + "\tIndex: " + index);
+        }
+    }
+}
